848c7ae260d594ddc4d62a5d8b690da41ec27423
Aditya Revankar <adityarevankar03@gmail.com>
2023-07-13 23:19:08 +0530

    aboutMe

--- vrn/client/src/App.js
+++ vrn/client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/home.jsx";
+import AboutMe from "./pages/aboutMe.jsx";
 function App() {
   return (
     <div className="App">
       <Home />
+      <AboutMe />
     </div>
   );
 }
---
+++ vrn/client/src/pages/aboutMe.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./aboutMe.css";
+import img from './imgtwo.jpeg';
+import imgtwo from './tree.jpg';
+
+function AboutMe() {
+  return (
+    <div className="container">
+        <div className="card-container">
+            
+            <div className="image-container"><img src={img} alt="" class="card-img"/></div>
+            <div className="sub-heading">Data-Driven Decisions</div>
+            <div className="info-text">Using cutting-edge analytics,we optimisze your marketing strategies,amplifying your impact and ensuring a solid ROI.</div>
+
+        </div>
+        <div className="card-container">
+            <div className="image-container"><img src={imgtwo} alt="" class="card-img"/></div>
+            <div className="sub-heading">Client Satisfaction</div>
+            <div className="info-text">Your success is our mission.We're committed to exceeding your expectations adn earning your trust.</div>
+
+        </div>
+        <div className="card-container">
+            <div className="image-container"><img src={img} alt="" class="card-img"/></div>
+            <div className="sub-heading">Ongoing Support</div>
+            <div className="info-text">Our partnership doesn't end with a campaingn-we offer comprehensive support to ensure your contineud growth and success</div>
+
+        </div>
+    </div>
+  );
+}
+export default AboutMe;
