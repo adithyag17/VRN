1e5995b3e64371ac10faae4ee60ff67e350a4484
adithyag17 <97718839+adithyag17@users.noreply.github.com>
2023-07-13 22:53:18 +0530

    vanta

--- vrn/client/src/pages/home.jsx
+++ vrn/client/src/pages/home.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./home.css";
+import NET from "vanta/dist/vanta.net.min";
+import * as THREE from "three";
+
 function Home() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          color: 0x888888, // Set the color to gray
+          backgroundColor: 0xe5e0ea,
+          maxDistance: 34.0,
+        })
+      );
+    }
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
     <div className="container">
       <div className="dashboard">
@@ -9,6 +33,7 @@ function Home() {
         <button>Partners</button>
         <button>Contact Us</button>
       </div>
+      <div className="vanta-background" ref={vantaRef}></div>
       <div className="center">
         <h1 className="heading slide-in">VRN</h1>
         <h1 className="heading2 slide-in">INNOVATORS</h1>
@@ -16,4 +41,5 @@ function Home() {
     </div>
   );
 }
+
 export default Home;
