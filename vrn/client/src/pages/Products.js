d80d1e8fa8340cd4714d332e35521796ae41b23b
adithyag17 <97718839+adithyag17@users.noreply.github.com>
2023-07-22 13:01:47 +0530

    remove from cart

--- vrn/client/src/pages/Products.js
+++ vrn/client/src/pages/Products.js
@@ -35,44 +35,53 @@ const Products = () => {
       cost: 39.99,
       wattage: 30,
     },
-    
   ];
 
   const addToCart = (product) => {
     setCart((prevCart) => [...prevCart, product]);
   };
 
+  const removeFromCart = (productToRemove) => {
+    setCart((prevCart) =>
+      prevCart.filter((product) => product !== productToRemove)
+    );
+  };
+
   return (
     <div className="wrapper-parent">
       <div className="wrapper-child">
-
-      
-      <h1>Products</h1>
-      <div className="product-container">
-        
-        {productsData.map((product, index) => (
-
-          <div className="product-card" key={index}>
-            <div className="product-image"></div>
-            <h2 className="product-title">{product.product_name} &nbsp; <p className="product-cost">${product.cost}</p></h2>
-            
-            <p className="product-wattage">Wattage: {product.wattage}W</p>
-            <button className="product-add-btn" onClick={() => addToCart(product)}>Add to Cart</button>
-            
-            
-          </div>
-        ))}
-      </div>
-      <div className="cart-container">
-        <h2>Cart</h2>
-        {cart.map((product, index) => (
-          <div key={index}>
-            <h3>{product.product_name}</h3>
-            <p>Cost: ${product.cost}</p>
-            <p>Wattage: {product.wattage}W</p>
-            <hr />
-          </div>
-        ))}
+        <h1>Products</h1>
+        <div className="product-container">
+          {productsData.map((product, index) => (
+            <div className="product-card" key={index}>
+              <div className="product-image"></div>
+              <h2 className="product-title">
+                {product.product_name} &nbsp;{" "}
+                <p className="product-cost">${product.cost}</p>
+              </h2>
+              <p className="product-wattage">Wattage: {product.wattage}W</p>
+              <button
+                className="product-add-btn"
+                onClick={() => addToCart(product)}
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="cart-container">
+          <h2>Cart</h2>
+          {cart.map((product, index) => (
+            <div key={index}>
+              <h3>{product.product_name}</h3>
+              <p>Cost: ${product.cost}</p>
+              <p>Wattage: {product.wattage}W</p>
+              <button onClick={() => removeFromCart(product)}>
+                Remove from Cart
+              </button>
+              <hr />
+            </div>
+          ))}
         </div>
       </div>
     </div>
