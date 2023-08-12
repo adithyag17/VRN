6f537c8272d48cbf9aaef3485682c96cb9c1fd16
adithyag17 <97718839+adithyag17@users.noreply.github.com>
2023-08-12 16:13:44 +0530

    changes

--- vrn/SERVER/index.js
+++ vrn/SERVER/index.js
@@ -1,21 +1,56 @@
 import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
 import connectdb from "./db/connectToDB.js";
-//import createinvoice from "./routes/createinvoice";
+import mongoose from "mongoose";
+
 dotenv.config();
 const app = express();
 app.use(cors());
-//app.use("/api/v1/createinvoice", createinvoice);
-app.get("/", async (req, res) => {
-  res.send("hello from adithya");
+app.use(bodyParser.json());
+
+// Define a Mongoose schema for the JSON data
+const jsonSchema = new mongoose.Schema({
+  customerDetails: {
+    name: String,
+    address: String,
+    gstin: String,
+    email: String,
+  },
+  cart: [
+    {
+      product_name: String,
+      cost: Number,
+      wattage: Number,
+    },
+  ],
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+// Create a Mongoose model for the JSON data
+const JSONData = mongoose.model("JSONData", jsonSchema);
+
+app.post("/api/sendJSON", (req, res) => {
+  const receivedJSON = req.body;
+
+  // Print the received JSON object
+  console.log("Received JSON object:", receivedJSON);
+
+  // Respond to the client
+  res.json({ message: "JSON object received by the server" });
+});
+
 const startServer = async () => {
   try {
     connectdb(process.env.MONGODBURL);
-    app.listen(8081, () => console.log("Server has started on localhost:8080"));
+    app.listen(8081, () => console.log("Server has started on localhost:8081"));
   } catch (error) {
     console.log(error);
   }
 };
+
 startServer();
--- vrn/client/src/pages/Products.js
+++ vrn/client/src/pages/Products.js
@@ -1,52 +1,131 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Products.css"; // Import the CSS file
 
 const Products = () => {
   const [cart, setCart] = useState([]);
-
+  //const username = "adithyag17";
+  const [totalAmount, setTotalAmount] = useState(0);
+  const gstamount = useState(0);
+  const [customerDetails, setCustomerDetails] = useState({
+    name: "",
+    address: "",
+    gstin: "",
+    email: "",
+  });
+  const [showForm, setShowForm] = useState(false);
+  const [serialInvoiceNumber, setSerialInvoiceNumber] = useState(1);
+  const [productNumber, setProductNumber] = useState(1);
   const productsData = [
     {
       product_name: "LED Bulb",
-      cost: 10.99,
+      rate: 10.99,
       wattage: 9,
+      HSN: 222,
     },
     {
       product_name: "Fluorescent Tube",
-      cost: 14.99,
+      rate: 14.99,
       wattage: 18,
+      HSN: 222,
     },
     {
       product_name: "Halogens Spotlights",
-      cost: 19.99,
+      rate: 19.99,
       wattage: 20,
+      HSN: 222,
     },
     {
       product_name: "Smart LED Strip",
-      cost: 29.99,
+      rate: 29.99,
       wattage: 25,
+      HSN: 222,
     },
     {
       product_name: "Ceiling LED Panel",
-      cost: 39.99,
+      rate: 39.99,
       wattage: 30,
+      HSN: 222,
     },
     {
       product_name: "Ceiling LED Panel",
-      cost: 39.99,
+      rate: 39.99,
       wattage: 30,
+      HSN: 222,
     },
   ];
 
-  const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
-  };
-
-  const removeFromCart = (productToRemove) => {
-    setCart((prevCart) =>
-      prevCart.filter((product) => product !== productToRemove)
+  const handleAddToCart = (product) => {
+    const existingItem = cart.find(
+      (item) => item.product.product_name === product.product_name
     );
+    if (existingItem) {
+      setCart((prevCart) =>
+        prevCart.map((item) =>
+          item === existingItem
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart((prevCart) => [...prevCart, { product, quantity: 1 }]);
+    }
   };
 
+  const handleRemoveFromCart = (productToRemove) => {
+    const updatedCart = cart
+      .map((item) => {
+        if (item.product.product_name === productToRemove.product_name) {
+          return { ...item, quantity: item.quantity - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.quantity > 0);
+    setCart(updatedCart);
+  };
+
+  const generateJSON = () => {
+    setShowForm(true);
+  };
+  const calculateTotalAmount = () => {
+    const total = cart.reduce((accumulator, item) => {
+      return accumulator + item.product.rate * item.quantity;
+    }, 0);
+    setTotalAmount(total);
+  };
+  const handleSubmitForm = () => {
+    // Recalculate the total amount
+    const newTotalAmount = cart.reduce(
+      (total, item) => total + item.product.rate * item.quantity,
+      0
+    );
+
+    const dataToSend = {
+      serialInvoiceNumber,
+      productNumber,
+      customerDetails,
+      cart: cart.map((item) => ({ ...item.product, quantity: item.quantity })),
+      totalAmount: newTotalAmount.toFixed(2), // Include the recalculated total amount
+      gstamount: newTotalAmount * 0.18,
+      grandtotalAmount: newTotalAmount + newTotalAmount * 0.18,
+    };
+
+    // Send the JSON object to the server
+    axios
+      .post("http://localhost:8081/api/sendJSON", dataToSend)
+      .then((response) => {
+        console.log("JSON object sent to server:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error sending JSON object:", error);
+      });
+
+    // Increment serial invoice number and product number for the next JSON
+    setSerialInvoiceNumber(serialInvoiceNumber + 1);
+    setProductNumber(productNumber + 1);
+
+    setShowForm(false);
+  };
   return (
     <div className="wrapper-parent">
       <div className="wrapper-child">
@@ -56,13 +135,13 @@ const Products = () => {
             <div className="product-card" key={index}>
               <div className="product-image"></div>
               <h2 className="product-title">
-                {product.product_name} &nbsp;{" "}
-                <p className="product-cost">${product.cost}</p>
+                {product.product_name} &nbsp;
+                <p className="product-rate">${product.rate}</p>
               </h2>
               <p className="product-wattage">Wattage: {product.wattage}W</p>
               <button
                 className="product-add-btn"
-                onClick={() => addToCart(product)}
+                onClick={() => handleAddToCart(product)}
               >
                 Add to Cart
               </button>
@@ -71,18 +150,72 @@ const Products = () => {
         </div>
         <div className="cart-container">
           <h2>Cart</h2>
-          {cart.map((product, index) => (
+          {cart.map((item, index) => (
             <div key={index}>
-              <h3>{product.product_name}</h3>
-              <p>Cost: ${product.cost}</p>
-              <p>Wattage: {product.wattage}W</p>
-              <button onClick={() => removeFromCart(product)}>
+              <h3>{item.product.product_name}</h3>
+              <p>rate: ${item.product.rate}</p>
+              <p>Wattage: {item.product.wattage}W</p>
+              <p>Quantity: {item.quantity}</p>
+              <button onClick={() => handleRemoveFromCart(item.product)}>
                 Remove from Cart
               </button>
+              <button onClick={generateJSON}>Generate JSON</button>
               <hr />
             </div>
           ))}
         </div>
+        {showForm && (
+          <div className="form-container">
+            <h2>Customer Details Form</h2>
+            <form>
+              <label>Name:</label>
+              <input
+                type="text"
+                value={customerDetails.name}
+                onChange={(e) =>
+                  setCustomerDetails({
+                    ...customerDetails,
+                    name: e.target.value,
+                  })
+                }
+              />
+              <label>Address:</label>
+              <input
+                type="text"
+                value={customerDetails.address}
+                onChange={(e) =>
+                  setCustomerDetails({
+                    ...customerDetails,
+                    address: e.target.value,
+                  })
+                }
+              />
+              <label>GSTIN:</label>
+              <input
+                type="text"
+                value={customerDetails.gstin}
+                onChange={(e) =>
+                  setCustomerDetails({
+                    ...customerDetails,
+                    gstin: e.target.value,
+                  })
+                }
+              />
+              <label>Email:</label>
+              <input
+                type="text"
+                value={customerDetails.email}
+                onChange={(e) =>
+                  setCustomerDetails({
+                    ...customerDetails,
+                    email: e.target.value,
+                  })
+                }
+              />
+              <button onClick={handleSubmitForm}>Submit</button>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
